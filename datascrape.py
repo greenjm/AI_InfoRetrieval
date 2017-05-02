149031bd0aaf75ea08f7e162df5e481c43ed7af8
Austin Niccum <xniccum@gmail.com>
2017-05-01 21:54:25 -0400

    added prop file

--- datascrape.py
+++ datascrape.py
@@ -22,16 +22,15 @@ for name in glob.glob("*.html"):
 			html = f.read()
 			parsed_html = BeautifulSoup(html)
 			temp = open(writeDir+'/'+name.replace('.html',''),'w')
-			s = parsed_html.title.string.replace(' - Wikipedia, the free encyclopedia','') + parsed_html.body.getText().encode('utf-8')
+			s = parsed_html.title.string.replace(' - Wikipedia, the free encyclopedia','').encode('utf-8') + '\n' + parsed_html.body.getText().encode('utf-8')
 			length += len(s)
 			temp.write(s)
 			temp.close()
-			i += 1
 	except IOError as exc:
 		if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
 			raise
 
 temp = open(writeDir+'/properties','w')
-temp.write('N '+str(i))
+temp.write('N '+str(i)+'\n')
 temp.write('avg '+str(length/i))
 
